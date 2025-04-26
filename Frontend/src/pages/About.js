6598511251763c660497997bb770813f8c261933
Savannah Alanis <savannahalanis2004@gmail.com>
2025-04-25 21:03:32 -0700

    basics

---
+++ Frontend/src/pages/About.js
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+
+function About() {
+  return (
+    <div>
+      <Navbar />
+      <h1>About Us</h1>
+      <p>This is the About page.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ Frontend/src/pages/Play.js
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+
+function Play() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Play Page</h1>
+      <p>Here is where most of the functionality will go.</p>
+    </div>
+  );
+}
+
+export default Play;
