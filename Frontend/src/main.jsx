5fe2a5c1a1b5322e5a271af688606b37d09828f3
Cathleen Qiao <cathleenqiao@gmail.com>
2025-04-25 21:16:46 -0700

    ok it's working now

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-const root = ReactDOM.createRoot(document.getElementById('app'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ import { createRoot } from 'react-dom/client'
+ import './index.css'
+ import App from './App.jsx'
+ 
+ createRoot(document.getElementById('root')).render(
+   <StrictMode>
+     <App />
+   </StrictMode>,
+ )
